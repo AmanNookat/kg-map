62cdb4e49017334a84ef196c3d20e58e5755f30e
AmanNookat <amantur.mendibaev2003@gmail.com>
2024-04-30 14:37:34 +0600

    initial

---
+++ src/pages/region-page/index.tsx
@@ -0,0 +1,3 @@
+export const RegionPage = () => {
+  return <div>RegionPage</div>;
+};
---
+++ src/routes.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import { HomePage } from "./pages/home-page";
+import { RegionPage } from "./pages/region-page";
+
+export const appRouter = createBrowserRouter([
+  {
+    errorElement: <h1>Error</h1>,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/:region",
+        element: <RegionPage />,
+      },
+    ],
+  },
+]);
